85327b366e6e4a536d48f360e34b89b4543e9e39
Leonardo Barraza <leobarrazat@gmail.com>
2018-10-25 01:04:42 -0300

    Crea html basico de galeria
    Crea el html basico de galeria ademas de vistas y urls

--- misdoges/views.py
+++ misdoges/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Perro
 
-# Create your views here.
+def gallery(request):
+    perros = Perro.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/gallery.html', {'perros': perros})
