9aaf85649e6aa3e833dcc9d946f7c2531e011a2c
Leonardo Barraza <leobarrazat@gmail.com>
2018-10-28 22:46:20 -0300

    Modifica css galeria y models.py
    modifica el css de la galeria y agrega elemetos a  la clase perro.

--- misdoges/models.py
+++ misdoges/models.py
@@ -1,11 +1,18 @@
 from django.db import models
 from django.utils import timezone
 
+EstadoPerro = {
+    ('Rescatado', 'Rescatado'),
+    ('Adoptado', 'Adoptado'),
+    ('Disponible', 'Disponible')
+}
+
 class Perro(models.Model):
+    idperro = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=50)
     raza = models.CharField(max_length=50)
     descripcion = models.CharField(max_length=200)
-    estado = models.CharField(max_length=11)
+    estado = models.CharField(max_length=15, choices=EstadoPerro)
     foto = models.FileField()
     created_date = models.DateTimeField(
             default=timezone.now)
